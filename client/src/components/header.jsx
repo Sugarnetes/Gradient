e8f6c179b62ecff116648f4e271e6e09d0bef21f
kevin-was-here <code --wait>
2024-01-06 15:16:58 -0700

    header.jsx

---
+++ client/src/components/header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+const Header = () => {
+    const handleLogout = () => {
+        // Handle logout logic here
+    };
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    My App
+                </Typography>
+                <Typography variant="body1" sx={{ marginRight: 2 }}>
+                    Username
+                </Typography>
+                <Button color="inherit" onClick={handleLogout}>
+                    Logout
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+        
+        <div>
+            
+        </div>
+    );
+}
+
+export default Home;

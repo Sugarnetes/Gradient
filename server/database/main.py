002e52d5b05585ce4ac7e7382987b92cca843869
Bach <waridh@ualberta.ca>
2024-01-06 14:55:51 -0700

    updated the gitignore to ignore the firebase credentials

--- server/database/main.py
+++ server/database/main.py
@@ -1,1 +1,7 @@
 import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate()
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()

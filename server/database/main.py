01c0f3a1f38bce49ca3e698df182f9604cb21b16
Bach <waridh@ualberta.ca>
2024-01-06 15:24:47 -0700

    made the basic version of the database handler

--- server/database/main.py
+++ server/database/main.py
@@ -2,9 +2,18 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("../../firebase_credentials/hacked24-60c88-firebase-adminsdk-5fu9m-0ba7ceb240.json")
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
 
-doc_ref = db.collection("users").document("alovelace")
-doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+class DatabaseHandler:
+    def __init__(self):
+        self.cred = credentials.Certificate("../../firebase_credentials/hacked24-60c88-firebase-adminsdk-5fu9m-0ba7ceb240.json")
+        self.app = firebase_admin.initialize_app(self.cred)
+        self.db = firestore.client()
+    
+    def user_collection(self):
+        return self.db.collection("users")
+
+
+if __name__ == "__main__":
+    db_handler = DatabaseHandler()
+    doc_ref = db_handler.user_collection().document("testing")
+    doc_ref.set({"first": "Bach"})

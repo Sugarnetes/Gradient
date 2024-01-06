2ff556d688a2ed5c3d23a167bbe40a8a14e2005f
Bach <waridh@ualberta.ca>
2024-01-06 15:04:55 -0700

    finished minimal working database communication

--- server/database/main.py
+++ server/database/main.py
@@ -2,6 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate()
+cred = credentials.Certificate("../../firebase_credentials/hacked24-60c88-firebase-adminsdk-5fu9m-0ba7ceb240.json")
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+doc_ref = db.collection("users").document("alovelace")
+doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})

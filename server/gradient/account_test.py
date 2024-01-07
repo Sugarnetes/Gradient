d09bc0a1107b8e74c1329bcc914817826731d145
Bach <waridh@ualberta.ca>
2024-01-07 10:37:51 -0700

    updated the unit test a little to include the new functions

--- server/gradient/account_test.py
+++ server/gradient/account_test.py
@@ -8,6 +8,12 @@ class MyTestCase(unittest.TestCase):
         expected = Account("testing1")
         test_input = {"username": "testing1", "time_spent": 0, "points": 0}
         self.assertEqual(expected, Account.from_dict(test_input))
+    
+    def test_from_dict(self):
+        expected = Account("testing1", 0, 0)
+        test_input = {"username": "testing1", "time_spent": 0, "points": 0}
+        self.assertEqual(expected, Account.from_dict(test_input))
+        
 
 if __name__ == '__main__':
     unittest.main()
